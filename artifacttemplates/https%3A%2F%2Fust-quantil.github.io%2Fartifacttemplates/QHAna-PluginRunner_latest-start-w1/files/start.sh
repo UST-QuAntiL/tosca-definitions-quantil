bfca31b2c8dff8625eee11b54a4b1fb8ed421a25
Lukas Harzenetter <lharzenetter@gmx.de>
2021-08-03 16:59:17 +0200

    add Guniconrn App

--- artifacttemplates/https%3A%2F%2Fust-quantil.github.io%2Fartifacttemplates/QHAna-PluginRunner_latest-start-w1/files/start.sh
+++ artifacttemplates/https%3A%2F%2Fust-quantil.github.io%2Fartifacttemplates/QHAna-PluginRunner_latest-start-w1/files/start.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+python3 -m gunicorn --daemon --workers 4 --bind 0.0.0.0:$Port "$AppModule"
+echo "started gunicorn demon on $Port with application $AppModule"
+
 python3 -m celery --app qhana_plugin_runner.celery_worker:CELERY worker --loglevel INFO --autoscale $MinWorkers,$MaxWorkers --detach
+echo "started QHAna application!"
