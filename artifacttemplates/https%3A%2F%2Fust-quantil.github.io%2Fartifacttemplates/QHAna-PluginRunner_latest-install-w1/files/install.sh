90182ef8ee5385ea6aa1ae00cc8d1bb4165bd805
Marvin Bechtold <marvin.bechtold.dev@gmail.com>
2021-09-23 19:00:08 +0200

    Add connectTo for PluginRunner
    Signed-off-by: Marvin Bechtold <marvin.bechtold.dev@gmail.com>

--- artifacttemplates/https%3A%2F%2Fust-quantil.github.io%2Fartifacttemplates/QHAna-PluginRunner_latest-install-w1/files/install.sh
+++ artifacttemplates/https%3A%2F%2Fust-quantil.github.io%2Fartifacttemplates/QHAna-PluginRunner_latest-install-w1/files/install.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 python3 -m pip install git+https://github.com/UST-QuAntiL/qhana-plugin-runner.git#egg=qhana_plugin_runner
+python3 -m pip install PyMySQL
--- artifacttemplates/https%3A%2F%2Fust-quantil.github.io%2Fartifacttemplates/QHAna-PluginRunner_latest-start-w1/files/start.sh
+++ artifacttemplates/https%3A%2F%2Fust-quantil.github.io%2Fartifacttemplates/QHAna-PluginRunner_latest-start-w1/files/start.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+cat << EOF > start_script.sh
+#!/bin/bash
+FLASK_APP=qhana_plugin_runner
+FLASK_ENV=production
+
+python3 -m flask create-db
+
 python3 -m gunicorn --daemon --workers 4 --bind 0.0.0.0:$Port "$AppModule"
 echo "started gunicorn demon on $Port with application $AppModule"
 
 python3 -m celery --app qhana_plugin_runner.celery_worker:CELERY worker --loglevel INFO --autoscale $MinWorkers,$MaxWorkers --detach
 echo "started celery with $MinWorkers, $MaxWorkers"
 echo "started QHAna application!"
+EOF
