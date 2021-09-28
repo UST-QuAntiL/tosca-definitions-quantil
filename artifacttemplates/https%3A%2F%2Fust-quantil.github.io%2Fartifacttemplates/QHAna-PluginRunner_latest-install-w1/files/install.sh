bddd1ecdd7032e1f7e1e47057e20487349407d76
Lukas Harzenetter <lharzenetter@gmx.de>
2021-09-28 17:47:57 +0200

    QHAna (#8)
    * Add connectTo for PluginRunner
    
    Signed-off-by: Marvin Bechtold <marvin.bechtold.dev@gmail.com>
    
    * Fix PluginRunner
    
    Signed-off-by: Marvin Bechtold <marvin.bechtold.dev@gmail.com>
    
    * Add namespace  "https://ust-quantil.github.io/quantum/applications/nodetypes"
    
    Signed-off-by: Marvin Bechtold <marvin.bechtold.dev@gmail.com>
    
    * fix qhana plugin runner implemenation
    
    * remove wrong namespaces
    
    * remove connect tos
    
    * fix quantil
    
    * Fix remove unused operations from PluginRunner
    
    Signed-off-by: Marvin Bechtold <marvin.bechtold.dev@gmail.com>
    
    * Add capability to add plugins as DAs
    
    Signed-off-by: Marvin Bechtold <marvin.bechtold.dev@gmail.com>
    
    Co-authored-by: Marvin Bechtold <marvin.bechtold.dev@gmail.com>

--- artifacttemplates/https%3A%2F%2Fust-quantil.github.io%2Fartifacttemplates/QHAna-PluginRunner_latest-install-w1/files/install.sh
+++ artifacttemplates/https%3A%2F%2Fust-quantil.github.io%2Fartifacttemplates/QHAna-PluginRunner_latest-install-w1/files/install.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-python3 -m pip install git+https://github.com/UST-QuAntiL/qhana-plugin-runner.git#egg=qhana_plugin_runner
+apt-get update -qq
+apt-get install git -qq
+apt-get -qqy install unzip;
+python3 -m pip install git+https://github.com/UST-QuAntiL/qhana-plugin-runner.git#egg=qhana_plugin_runner --quiet 
+python3 -m pip install PyMySQL
+
+IFS=';' read -ra NAMES <<< "$DAs";
+for i in "${NAMES[@]}"; do
+    IFS=',' read -ra entry <<< "$i";
+  # find the zip file
+    if [[ "${entry[1]}" == *.zip ]];
+    then
+        # unzip the application to
+        sudo unzip $CSAR${entry[1]} -d plugins
+    fi
+done
+
+echo "installed PluginRunner with plugins"
