64669ffaed90d3c5846bebd6629b8b99e20b1a40
Marvin Bechtold <marvin.bechtold.dev@gmail.com>
2022-08-12 10:10:56 +0200

    Update PluginRunner (#11)

---
+++ artifacttemplates/https%3A%2F%2Fust-quantil.github.io%2Fartifacttemplates/QHana-Plugin-Runner-Python_start-w1/files/start.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import sys
+from pathlib import Path
+from os import environ
+from typing import ChainMap
+
+def argv_to_dict(argv):
+    return dict(arg.split('=', maxsplit=1) for arg in argv if '=' in arg)
+
+def write_start_script(environ):
+    port = environ.get("GunicornAppPort", "5000")
+    app_module = environ.get("AppModule", "qhana_plugin_runner:create_app()")
+    min_workers = environ.get("MinWorkers", "1")
+    max_workers = environ.get("MaxWorkers", "2")
+
+    script_content = f'''#!/bin/bash
+export FLASK_APP=qhana_plugin_runner
+export FLASK_ENV=production
+export PLUGIN_FOLDERS=plugins
+export URL_MAP='{{"(?P<scheme>https?://)(localhost|host.docker.internal):(?P<port>[0-9]+)": "\\\\g<scheme>host.docker.internal:\\\\g<port>"}}'
+
+python3 -m flask create-db
+
+python3 -m gunicorn --daemon --workers 4 --bind 0.0.0.0:{port} "{app_module}"
+echo "started gunicorn demon on {port} with application {app_module}"
+
+python3 -m celery --app qhana_plugin_runner.celery_worker:CELERY worker --loglevel INFO --autoscale {min_workers},{max_workers} --detach
+echo "started celery with {min_workers}, {max_workers}"
+echo "started QHAna application!"
+sleep 10
+    '''
+
+    with Path('start_script.sh').open(mode='wt') as start_script:
+        start_script.write(script_content)
+
+def main(argv):
+    print(argv)
+    env = ChainMap(argv_to_dict(argv), environ)
+    write_start_script(env)
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
