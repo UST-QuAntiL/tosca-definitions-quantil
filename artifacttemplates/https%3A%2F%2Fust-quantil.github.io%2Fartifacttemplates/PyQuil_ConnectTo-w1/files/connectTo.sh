1c067525f59b574c6e63640f3bf89aada40eda44
wederbn <benjamin.weder@iaas.uni-stuttgart.de>
2022-10-19 15:08:48 +0200

    Fix typos

--- artifacttemplates/https%3A%2F%2Fust-quantil.github.io%2Fartifacttemplates/PyQuil_ConnectTo-w1/files/connectTo.sh
+++ artifacttemplates/https%3A%2F%2Fust-quantil.github.io%2Fartifacttemplates/PyQuil_ConnectTo-w1/files/connectTo.sh
@@ -3,5 +3,5 @@
 sudo mkdir -p pyquil_app
 
 echo "QPU_NAME=${QPU_NAME}" >> pyquil_app/.env
-echo "QVM_HOSTNAME"=${TARGET_CointainerIP} >> pyquil_app/.env
+echo "QVM_HOSTNAME"=${TARGET_ContainerIP} >> pyquil_app/.env
 echo "QVM_PORT"=${QVM_PORT} >> pyquil_app/.env
