b1e558d451a507e1558a18c51a326827b03aea84
Daniel Vietz <vietzd@gmail.com>
2022-10-21 10:25:43 +0200

    Make install script of Rigetti QVM non-interactive

--- artifacttemplates/https%3A%2F%2Fust-quantil.github.io%2Fartifacttemplates/RigettiQVM-install-IA/files/install.sh
+++ artifacttemplates/https%3A%2F%2Fust-quantil.github.io%2Fartifacttemplates/RigettiQVM-install-IA/files/install.sh
@@ -8,11 +8,12 @@ sudo apt-get update -qq
 
 #pip install pyquil
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y python3-pip wget -qq
+# download and install rigetti forest sdk
+DEBIAN_FRONTEND=noninteractive apt-get install -y python3-pip wget -qq
 wget https://downloads.rigetti.com/qcs-sdk/forest-sdk-2.23.0-linux-deb.tar.bz2
 tar xjf forest-sdk-2.23.0-linux-deb.tar.bz2
 cd forest-sdk-2.23.0-linux-deb
-sudo ./forest-sdk-2.23.0-linux-deb.run
+yes | ./forest-sdk-2.23.0-linux-deb.run
 
 # fix wrong versions referenced by qvm
 wget http://archive.ubuntu.com/ubuntu/pool/main/libf/libffi/libffi6_3.2.1-8_amd64.deb
