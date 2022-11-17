41b0154027455537aeed8fa85cbb0fdece639186
Philipp Wundrack <philipp.wundrack@live.de>
2022-11-17 16:43:32 +0100

    rename service endpoint and port type

--- artifacttemplates/https%3A%2F%2Fust-quantil.github.io%2Fartifacttemplates/PlanQK-Platform-IA_w1-wip1/source/src/main/java/org/opentosca/artifacttemplates/dockercontainer/DockerContainerConstants.java
+++ artifacttemplates/https%3A%2F%2Fust-quantil.github.io%2Fartifacttemplates/PlanQK-Platform-IA_w1-wip1/source/src/main/java/org/opentosca/artifacttemplates/dockercontainer/DockerContainerConstants.java
@@ -6,7 +6,7 @@ public abstract class DockerContainerConstants {
     public static final String NAMESPACE_URI = "http://artifacttemplates.opentosca.org";
 
     // Port type to use for the SOAP service
-    public static final String PORT_TYPE_NAME = "org_opentosca_artifactTemplates_DockerContainer_ContainerManagementInterfacePort";
+    public static final String PORT_TYPE_NAME = "io_github_ust-quantil_artifactTemplates_PlanQK-Platform_ContainerInterfacePort";
 
     // Name of the XML schema file to use as basis for the WSDL generation
     public static final String XSD_NAME = "containerManagementInterface.xsd";
