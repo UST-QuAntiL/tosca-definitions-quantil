a54fe6a67ac6d0e5dae2e46d8b05ef3a541bcd7c
Philipp Wundrack <philipp.wundrack@live.de>
2022-12-05 11:29:39 +0100

    add service creation implementation, fix termination operation

--- artifacttemplates/https%3A%2F%2Fust-quantil.github.io%2Fartifacttemplates/PlanQK-Platform-IA_w1-wip1/source/ia/src/main/java/org/opentosca/artifacttemplates/dockercontainer/DockerContainerManagementInterfaceEndpoint.java
+++ artifacttemplates/https%3A%2F%2Fust-quantil.github.io%2Fartifacttemplates/PlanQK-Platform-IA_w1-wip1/source/ia/src/main/java/org/opentosca/artifacttemplates/dockercontainer/DockerContainerManagementInterfaceEndpoint.java
@@ -19,8 +19,6 @@ import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 
 import static org.opentosca.artifacttemplates.dockercontainer.FileHandler.downloadFile;
-import static org.opentosca.artifacttemplates.dockercontainer.FileHandler.getFile;
-import static org.opentosca.artifacttemplates.dockercontainer.FileHandler.getUrl;
 
 import de.stoneone.planqk.api.invoker.ApiClient;
 
@@ -36,20 +34,103 @@ public class DockerContainerManagementInterfaceEndpoint {
         SoapUtil.logHeaders(messageContext);
         OpenToscaHeaders openToscaHeaders = SoapUtil.parseHeaders(messageContext);
 
-        String apiKey = "";  // TODO: get api key
+        String userCodeURL = null;
+
+        for (var namespaces : openToscaHeaders.deploymentArtifacts().entrySet()) {
+            for (var da : namespaces.getValue().entrySet()) {
+                if (da.getKey().equals("user_code.zip")) {
+                    userCodeURL = da.getValue();
+                }
+            }
+        }
+
+        if (userCodeURL == null) {
+            LOG.error("no DA found with file name user_code.zip");
+
+            InvokeResponse invokeResponse = new InvokeResponse();
+            invokeResponse.setMessageID(openToscaHeaders.messageId());
+            invokeResponse.setResult("Error: no DA found with file name user_code.zip");
+
+            SoapUtil.sendSoapResponse(invokeResponse, InvokeResponse.class, openToscaHeaders.replyTo());
+            return;
+        }
+
+        LOG.info("found DA with file name user_code.zip");
+        LOG.info("downloading user code");
+        File userCode = downloadFile(userCodeURL);
+
+        if (userCode == null) {
+            InvokeResponse invokeResponse = new InvokeResponse();
+            invokeResponse.setMessageID(openToscaHeaders.messageId());
+            invokeResponse.setResult("Error: could not download user_code.zip");
+
+            SoapUtil.sendSoapResponse(invokeResponse, InvokeResponse.class, openToscaHeaders.replyTo());
+            return;
+        }
+
+        LOG.info("size of downloaded user code: {}", userCode.length());
+
+        String apiDefinitionURL = null;
+
+        for (var namespaces : openToscaHeaders.deploymentArtifacts().entrySet()) {
+            for (var da : namespaces.getValue().entrySet()) {
+                if (da.getKey().equals("api_definition.json")) {
+                    apiDefinitionURL = da.getValue();
+                }
+            }
+        }
+
+        File apiDefinition = null;
+
+        if (apiDefinitionURL == null) {
+            LOG.info("Optional DA with file name api_definition.json not found.");
+        } else {
+            LOG.info("Optional DA with file name api_definition.json found.");
+            LOG.info("downloading api_definition.json");
+            apiDefinition = downloadFile(apiDefinitionURL);
+
+            if (apiDefinition == null) {
+                LOG.info("could not download api_definition.json");
+            } else {
+                LOG.info("size of downloaded api_definition.json: {}", apiDefinition.length());
+            }
+        }
+
+        String apiKey = request.getPlanqkApiKey();
+
+        if (apiKey.equals("")) {
+            LOG.error("API key is empty");
+
+            InvokeResponse invokeResponse = new InvokeResponse();
+            invokeResponse.setMessageID(openToscaHeaders.messageId());
+            invokeResponse.setResult("Error: API key is empty");
+
+            SoapUtil.sendSoapResponse(invokeResponse, InvokeResponse.class, openToscaHeaders.replyTo());
+            return;
+        }
+
         ApiClient apiClient = new ApiClient("apiKey", apiKey);
         apiClient.setFeignBuilder(apiClient.getFeignBuilder().decoder(new CustomDecoder(apiClient.getObjectMapper())));
 
         ServicePlatformServicesApi servicesApi = apiClient.buildClient(ServicePlatformServicesApi.class);
 
-        String serviceName = "Test service";  // TODO: get service name
+        String serviceName = request.getServiceName();
         String type = "MANAGED";
-        String description = "service description";  // TODO: get service description
-        File userCode = new File(""); // TODO: get user code file
-        File apiDefinition = new File("");  // TODO: get api definition file
-        String quantumBackend = "NONE";
+        String description = request.getServiceDescription();
+        String quantumBackend = request.getQuantumBackend();
 
-        // ServiceDto service = servicesApi.createService(serviceName, type, quantumBackend, description, null, null, userCode, apiDefinition);  // TODO: update arguments to new version of the api client
+        ServiceDto service = servicesApi.createService(
+                serviceName,
+                type,
+                quantumBackend,
+                description,
+                null,
+                null,
+                null,
+                null,
+                null,
+                userCode,
+                apiDefinition);
 
         InvokeResponse invokeResponse = new InvokeResponse();
         invokeResponse.setMessageID(openToscaHeaders.messageId());
@@ -64,7 +145,12 @@ public class DockerContainerManagementInterfaceEndpoint {
 
         SoapUtil.logHeaders(messageContext);
         OpenToscaHeaders openToscaHeaders = SoapUtil.parseHeaders(messageContext);
+
+        // TODO: delete service
+
         InvokeResponse invokeResponse = new InvokeResponse();
+        invokeResponse.setMessageID(openToscaHeaders.messageId());
+        invokeResponse.setResult("finished");
 
         // Send response
         SoapUtil.sendSoapResponse(invokeResponse, InvokeResponse.class, openToscaHeaders.replyTo());
--- artifacttemplates/https%3A%2F%2Fust-quantil.github.io%2Fartifacttemplates/PlanQK-Platform-IA_w1-wip1/source/ia/src/main/java/org/opentosca/artifacttemplates/dockercontainer/FileHandler.java
+++ artifacttemplates/https%3A%2F%2Fust-quantil.github.io%2Fartifacttemplates/PlanQK-Platform-IA_w1-wip1/source/ia/src/main/java/org/opentosca/artifacttemplates/dockercontainer/FileHandler.java
@@ -1,57 +1,71 @@
 package org.opentosca.artifacttemplates.dockercontainer;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.io.OutputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URLConnection;
 
-import org.apache.commons.io.FileUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import com.google.common.io.Files;
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
+import org.apache.commons.io.IOUtils;
 
 public class FileHandler {
 
-    private static final Logger LOG = LoggerFactory.getLogger(FileHandler.class);
-
-    static URL getUrl(String url) {
+    protected static File downloadFile(final String url) {
         try {
-            return new URL(url);
-        } catch (MalformedURLException e) {
-            return null;
-        }
-    }
+            final URI dockerImageURI = new URI(url);
 
-    static File getFile(String path) {
-        File file = new File(path);
-        if (file.exists()) {
-            return file;
+            final String[] pathSplit = dockerImageURI.getRawPath().split("/");
+            final String fileName = pathSplit[pathSplit.length - 1];
+
+            final File tempDir = Files.createTempDir();
+            final File tempFile = new File(tempDir, fileName);
+
+            downloadFile(dockerImageURI, tempFile);
+            return tempFile;
+        } catch (final URISyntaxException | IOException e) {
+            e.printStackTrace();
         }
+
         return null;
     }
 
-    static String downloadFile(URL url, String directory, String filename) throws IOException {
-        HttpURLConnection connection = null;
-        InputStream stream = null;
+    protected static void downloadFile(URI dockerImageURI, File tempFile) throws IOException {
+        final URLConnection connection = dockerImageURI.toURL().openConnection();
+        connection.setRequestProperty("Accept", "application/octet-stream");
 
-        try {
-            connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestProperty("Accept", "application/octet-stream");
-            stream = connection.getInputStream();
+        try (final InputStream input = connection.getInputStream()) {
+            final byte[] buffer = new byte[4096];
+            int n;
 
-            File file = new File(directory, filename);
-            FileUtils.copyInputStreamToFile(stream, file);
-            LOG.info("Downloaded file '{}' to temporary file '{}'. Didn't I? {}", url, file, file.exists());
-
-            return file.toString();
-        } finally {
-            if (stream != null) {
-                stream.close();
-            }
-            if (connection != null) {
-                connection.disconnect();
+            try (final OutputStream output = new FileOutputStream(tempFile)) {
+                while ((n = input.read(buffer)) != -1) {
+                    output.write(buffer, 0, n);
+                }
             }
         }
+    }
+
+    protected static File createTempTarFromFile(final File file) {
+        final TarArchiveEntry entry = new TarArchiveEntry(file, file.getName());
+
+        File tarArchive = null;
+        try {
+            tarArchive = File.createTempFile(String.valueOf(System.currentTimeMillis()), ".tar");
+            final TarArchiveOutputStream out = new TarArchiveOutputStream(new FileOutputStream(tarArchive));
+            out.putArchiveEntry(entry);
+            IOUtils.copy(new FileInputStream(file), out);
+            out.closeArchiveEntry();
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
+
+        return tarArchive;
     }
 }
